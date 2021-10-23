bf59af4f05e39704ff8d56eebddb4a46fbeaba21
Tyler Ross <tross020890@gmail.com>
2021-10-22 21:12:20 -0500

    Add variables

---
+++ variables.rb
@@ -0,0 +1,9 @@
+famous_person = "George Carlin"
+is_active = false
+famous_person = "Bill Hicks"
+num_3 = 3
+num_9 = 9
+sum = num_3 + num_9
+animals = ["otter", "panda", "goat"]
+animals.delete_at(0)
+puts "animals".length
