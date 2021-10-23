eaa08c4ac7705f5127b876e1277e22db3e2d2ce4
Tyler Ross <tross020890@gmail.com>
2021-10-22 21:31:07 -0500

    Add model b

---
+++ models/model_b.rb
@@ -0,0 +1,12 @@
+VEHICLE = {
+  make: "Lotus",
+  model: "Emira",
+  model_year: 2022,
+  horsepower: 400,
+  is_common: false,
+  is_expensive: true,
+  colors: ["red", "green", "yellow", "blue", "gray"],
+  interior: ["leather", "alcantara"],
+
+
+}
