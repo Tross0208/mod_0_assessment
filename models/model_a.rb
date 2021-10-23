f28dac3d46d79412c2c6fab48c133143578882df
Tyler Ross <tross020890@gmail.com>
2021-10-22 21:20:40 -0500

    Add model

---
+++ models/model_a.rb
@@ -0,0 +1,12 @@
+AIRPORT = {
+  name: "ORD",
+  city: "Chicago",
+  airlines: 20,
+  distance_in_miles: 30,
+  is_big: true,
+  is_fun: false,
+  contraband: ["weapons", "narcotics"],
+  main_airlines: ["United", "American"],
+  
+
+}
